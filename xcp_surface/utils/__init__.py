373ab6ce9ceace6ba4bf1c6ae025d56e395de8a8
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2020-11-22 22:21:37 -0500

    sas

--- xcp_surface/utils/__init__.py
+++ xcp_surface/utils/__init__.py
@@ -2,9 +2,12 @@
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
 from .write_save import (read_ndata,write_ndata)
+from .plot import(plot_svg,compute_dvars)
 
 
 __all__ = [
     'read_ndata',
-    'write_ndata'
+    'write_ndata',
+    'plot_svg',
+    'compute_dvars'
 ]
