2d4f9ac36da536b086e4bd648347e62ccec2b432
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2020-11-22 22:27:16 -0500

    Update plot.py

--- xcp_surface/utils/plot.py
+++ xcp_surface/utils/plot.py
@@ -1,6 +1,6 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-"""Visualization tools."""
+"""ploting tools."""
 import numpy as np
 import nibabel as nb
 import pandas as pd
