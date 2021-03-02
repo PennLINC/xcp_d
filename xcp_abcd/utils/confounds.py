0d6b165137e909b78af2a5a52c02970b40b4109e
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2021-03-02 11:04:55 -0500

    update confound

--- xcp_abcd/utils/confounds.py
+++ xcp_abcd/utils/confounds.py
@@ -32,13 +32,18 @@ def readjson(jsonfile):
     return data
 
 
-def load_motion(confoundspd,head_radius):
+def load_motion(confoundspd,TR,filtertype=None,
+        cutoff=0.1,freqband=[0.1,0.2],order=4):
     """Load the 6 motion regressors."""
     head_radius = head_radius
     rot_2mm = confoundspd[["rot_x", "rot_y", "rot_z"]]
     trans_mm = confoundspd[["trans_x", "trans_y", "trans_z"]]
-
-    return pd.concat([rot_2mm,trans_mm],axis=1)   
+    datay = pd.concat([rot_2mm,trans_mm],axis=1).to_numpy()
+    if filtertype is not None:
+        fs=1/TR
+        datay = motion_regression_filter(data=datay,fs=fs,
+          filtertype=filtertype,cutoff=cutoff,freqband=freqband,order=order)
+    return  pd.DataFrame(datay) 
 
 def load_globalS(confoundspd):
     """select global signal."""
@@ -101,27 +106,80 @@ def load_confound_matrix(datafile,head_radius=50,params='24P'):
     '''
     confoundtsv,confoundjson = load_confound(datafile)
     if  params == '24P':
-        motion = load_motion(confoundtsv,head_radius=head_radius)
+        motion = load_motion(confoundtsv,TR,filtertype=None,
+        cutoff=0.1,freqband=[0.1,0.2],order=4)
         mm_dev = pd.concat([motion,derivative(motion)],axis=1)
         confound = pd.concat([mm_dev,confpower(mm_dev)],axis=1)
     elif  params == '27P':
-        motion = load_motion(confoundtsv,head_radius=head_radius)
+        motion = load_motion(confoundtsv,TR,filtertype=None,
+        cutoff=0.1,freqband=[0.1,0.2],order=4)
         mm_dev = pd.concat([motion,derivative(motion)],axis=1)
         wmcsf = load_WM_CSF(confoundtsv)
         gs = load_globalS(confoundtsv)
         confound = pd.concat([mm_dev,confpower(mm_dev),wmcsf,gs],axis=1)
     elif params == '36P':
-        motion = load_motion(confoundtsv,head_radius=head_radius)
+        motion = load_motion(confoundtsv,TR,filtertype=None,
+        cutoff=0.1,freqband=[0.1,0.2],order=4)
         mm_dev = pd.concat([motion,derivative(motion)],axis=1)
         conf24p = pd.concat([mm_dev,confpower(mm_dev)],axis=1)
         gswmcsf = pd.concat([load_WM_CSF(confoundtsv),load_globalS(confoundtsv)],axis=1)
         gwcs_dev = pd.concat([gswmcsf,derivative(gswmcsf)],axis=1) 
         confound = pd.concat([conf24p,gwcs_dev,confpower(gwcs_dev)],axis=1)
     elif params == 'acompcor':
-        motion = load_motion(confoundtsv,head_radius=head_radius)
+        motion = load_motion(confoundtsv,TR,filtertype=None,
+        cutoff=0.1,freqband=[0.1,0.2],order=4)
         mm_dev = pd.concat([motion,derivative(motion)],axis=1)
         acompc = load_acompcor(confoundspd=confoundtsv, confoundjs=confoundjson)
         confound = pd.concat([mm_dev,acompc],axis=1)
     elif params == 'tcompcor':
         confound = load_tcompcor(confoundspd=confoundtsv,confoundjs=confoundjson)
     return confound
+
+
+
+def motion_regression_filter(data,fs,filtertype,cutoff,freqband,order=4):
+    """
+
+    """
+
+
+    from scipy.signal import firwin,iirnotch,filtfilt
+
+    def lowpassfilter_coeff(cutoff, fs, order=4):
+        
+        nyq = 0.5 * fs
+        fa = np.abs( cutoff - np.floor((cutoff + nyq) / fs) * fs)
+        normalCutoff = fa / nyq
+        b = firwin(order, cutoff=normalCutoff, window='hamming') 
+        a = 1
+        return b, a
+
+    def iirnortch_coeff(freqband,fs):
+        nyq = 0.5*fs
+        fa = np.abs(freqband - np.floor((np.add(freqband,nyq) / fs) * fs))
+        w0 = np.mean(fa)/nyq
+        bw = np.diff(fa)/nyq
+        qf = w0 / bw
+        b, a = iirnotch( w0, qf )
+        return b,a
+
+    if filtertype == 'lp':
+        b,a = lowpassfilter_coeff(cutoff,fs,order=4)
+    elif filtertype =='notch':
+        b,a = iirnortch_coeff(freqband,fs=fs)
+    
+    order_apply = np.int(np.floor(order/2))
+
+    for j in range(order_apply):
+        for k in range(data.shape[0]):
+            data[k,:] = filtfilt(b,a,data[k,:])
+        j=j+1
+    
+    return data 
+    
+    
+
+
+
+
+
