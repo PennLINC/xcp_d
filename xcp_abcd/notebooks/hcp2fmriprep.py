6cf28080cb2540dbd54c774df0d7069bb841b3f6
Maxwell Bertolero <mbertolero@me.com>
2021-07-07 12:17:42 -0400

    Update hcp2fmriprep.py
    add in fix compare code

--- xcp_abcd/notebooks/hcp2fmriprep.py
+++ xcp_abcd/notebooks/hcp2fmriprep.py
@@ -1,5 +1,5 @@
 #!~/anaconda3/bin/python
-import glob as glob
+import glob
 import os
 import sys
 import pandas as pd
@@ -9,6 +9,8 @@ from shutil import copyfile
 import json
 import subprocess
 import h5py
+from sklearn.linear_model import LinearRegression
+from scipy.stats import pearsonr
 
 tmpdir = subprocess.run(['echo $SBIA_TMPDIR'], stdout=subprocess.PIPE,shell=True).stdout.decode('utf-8').split('\n')[0]
 
@@ -293,3 +295,74 @@ if function == 'zipit':
 			data.create_dataset('bold/{0}/timeseries/{1}'.format(matrix['sub'],fname),m.shape,dtype=float,data=m)
 			for key in matrix.keys():
 				data['bold/{0}/timeseries/{1}'.format(matrix['sub'],fname)].attrs[key] = matrix[key]
+
+
+
+"""
+This is not really required but here is how I made the HCP ICA-FIX plus GSR to sanity check
+mean corr is 0.92
+"""
+def regression(data,confound):
+
+	'''
+	 data :
+	   numpy ndarray- vertices by timepoints
+	 confound:
+	   nuissance regressors reg by timepoints
+	 return:
+		residual matrix
+	'''
+	regr = LinearRegression()
+	regr.fit(confound,data)
+	y_pred = regr.predict(confound)
+
+	return data - y_pred
+
+
+def make_hcp():
+	os.system('rm /cbica/home/bertolem/xcp_hcp/WBS.txt')
+
+	for sub in glob.glob('/cbica/projects/HCP_Data_Releases/HCP_1200/**'):
+		subject = sub.split('/')[-1]
+		if subject == 'ToSync':
+			continue
+		files = glob.glob('/cbica/projects/HCP_Data_Releases/HCP_1200/{0}/MNINonLinear/Results/*rfMRI_REST*/rfMRI_**_Atlas_MSMAll_hp2000_clean.dtseries.nii'.format(subject))
+		m = []
+		for f in files:
+			run = f.split('/')[8]
+			surf = nib.load(f)
+			data = surf.get_fdata()
+			command = 'wb_command -cifti-stats {0} -reduce MEAN >> /cbica/home/bertolem/xcp_hcp/WBS.txt'.format(f)
+			os.system(command)
+			wbs = np.loadtxt('/cbica/home/bertolem/xcp_hcp/WBS.txt')
+			os.system('rm /cbica/home/bertolem/xcp_hcp/WBS.txt')
+			d = regression(data,wbs.reshape(-1,1))
+			new_img = nb.Cifti2Image(d, header=surf.header,nifti_header=surf.nifti_header)
+			cifti_out = "/cbica/home/bertolem/xcp_hcp/{0}_{1}.dtseries.nii".format(subject,run)
+			new_img.to_filename(cifti_out)
+			p= '/cbica/home/bertolem/yeo_parcels/Schaefer2018_400Parcels_17Networks_order.dlabel.nii'
+			out = '/cbica/home/bertolem/xcp_hcp/{0}_{1}_{2}.ptseries.nii'.format(subject,run,p.split('/')[-1].split('.')[0])
+			cmd =  'wb_command -cifti-parcellate {0} {1} COLUMN {2}'.format(cifti_out,p,out)
+			os.system(cmd)
+			os.system('rm -f {0}'.format(cifti_out))
+			ts = nb.load(out).get_fdata()
+			os.system('rm -f {0}'.format(out))
+			m.append(np.arctanh(np.corrcoef(ts.T)))
+		np.save('/cbica/home/bertolem/xcp_hcp/rest_fixgsr/{0}.npy'.format(subject),np.nanmean(m,axis=0))
+
+
+def compare():
+	corrs = []
+	idx = np.triu_indices(400,1)
+	hcp_matrices = glob.glob('/cbica/home/bertolem/xcp_hcp/rest_fixgsr/*npy*')
+	for m in hcp_matrices:
+		s = m[-10:-4]
+		hcp_m = np.load(m)
+		xcp = []
+		files = glob.glob('/cbica/home/bertolem/xcp_hcp/xcp_results/xcp_abcd/sub-{0}/func/*REST*pconn*'.format(s))
+		if len(files) == 0:continue
+		for f in files:
+			xcp.append(np.arctanh(nb.load(f).get_fdata()))
+		if len(files) > 0: xcp = np.nanmean(xcp,axis=0)
+		corrs.append(pearsonr(xcp[idx],hcp_m[idx])[0])
+		print (np.mean(corrs))
