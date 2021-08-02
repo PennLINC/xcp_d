0c1c1e018b31274f55916b76337747839030d00c
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2021-08-02 19:52:35 -0400

    Update run.py

--- xcp_abcd/cli/run.py
+++ xcp_abcd/cli/run.py
@@ -275,7 +275,7 @@ def main():
             # Generate LaTex file resolving citations
             cmd = ['pandoc', '-s', '--bibliography',
                    pkgrf('xcp_abcd', 'data/boilerplate.bib'),
-                   '--citeproc', str(citation_files['md']),
+                   '--pandoc-citeproc', str(citation_files['md']),
                    '-o', str(citation_files['tex'])]
             logger.info('Generating a LaTeX version of the citation boilerplate...')
             try:
