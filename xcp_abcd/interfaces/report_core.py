2d2edbc11d94bff90dcc358868acd0d0e70e5108
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2021-10-03 20:56:02 -0400

    [skip ci]

--- xcp_abcd/interfaces/report_core.py
+++ xcp_abcd/interfaces/report_core.py
@@ -92,7 +92,8 @@ def generate_reports(
                    
         from .layout_builder import layout_builder 
         for subject_label in subject_list:
-            brainplotfile  = str(glob.glob(Path(output_dir)+ '/xcp_abcd/sub-{}'.format(subject_label)+'/figures/*_desc-brainplot_T1w.html')[0])
+            brainplotfile  = str(glob.glob(Path(output_dir)+ '/xcp_abcd/sub-'+ str(subject_label)+'/figures/*_desc-brainplot_T1w.html')[0])
+            print(brainplotfile)
             layout_builder(html_path=Path(output_dir)+'/xcp_abcd/', subject_id=subject_label,
                            session_id= _getsesid(brainplotfile))
 
