f96f0dae32a3f1a372ebec33b0fa8a6b351d8cd8
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2021-11-12 13:11:00 -0500

    [skip ci]

--- xcp_abcd/cli/run.py
+++ xcp_abcd/cli/run.py
@@ -378,11 +378,13 @@ def build_workflow(opts, retval):
         NIWORKFLOWS_LOG.info('Converting dcan to fmriprep format')
         dcan_output_dir = str(work_dir) + '/dcanhcp'
         os.makedirs(dcan_output_dir, exist_ok=True)
-        print(fmriprep_dir)
-        sub_id = dcan2fmriprep(fmriprep_dir,dcan_output_dir,sub_id=_prefix(str(opts.participant_label)))
-        print(sub_id)
-        if len(sub_id) > 0:
-            fmriprep_dir = dcan_output_dir
+       
+        if opts.participant_label is None:
+            dcan2fmriprep(fmriprep_dir,dcan_output_dir,sub_id=_prefix(str(opts.participant_label)))
+        else:
+            dcan2fmriprep(fmriprep_dir,dcan_output_dir)
+        fmriprep_dir = dcan_output_dir
+
         
     elif opts.input_type == 'hcp':
         opts.cifti = True
@@ -392,8 +394,13 @@ def build_workflow(opts, retval):
         hcp_output_dir = str(work_dir) + '/hcphcp'
         os.makedirs(hcp_output_dir, exist_ok=True)
         sub_id = hcp2fmriprep(fmriprep_dir,hcp_output_dir,sub_id=_prefix(str(opts.participant_label)))
-        if len(sub_id) > 0:
-            fmriprep_dir = hcp_output_dir
+         
+        if opts.participant_label is None:
+            hcp2fmriprep(fmriprep_dir,hcp_output_dir,sub_id=_prefix(str(opts.participant_label)))
+        else:
+            hcp2fmriprep(fmriprep_dir,hcp_output_dir)
+        fmriprep_dir = hcp_output_dir
+    
 
 
     # Set up some instrumental utilities
