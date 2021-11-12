5eac2da3f2f1546f1c1ee750ecfb70a9f084e616
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2021-11-12 12:22:42 -0500

    Update dcan2fmriprep.py

--- xcp_abcd/utils/dcan2fmriprep.py
+++ xcp_abcd/utils/dcan2fmriprep.py
@@ -84,7 +84,7 @@ def dcan2fmriprepx(dcan_dir,out_dir,sub_id):
 
         wmmask =glob.glob(anat_dirx + '/wm_2mm_*_mask_eroded.nii.gz')[0]
         csfmask =glob.glob(anat_dirx + '/vent_2mm_*_mask_eroded.nii.gz')[0]
-        tw1tonative = anat_dirx +'xfms/T1w_to_MNI_0GenericAffine.mat'
+        tw1tonative = wmmask
 
         # get task and idx  run 01 
         func_dirx  = dcan_dir +'/' + sub_id + '/ses-' +ses_id[0] + '/files/MNINonLinear/Results/'
@@ -181,6 +181,7 @@ def dcan2fmriprepx(dcan_dir,out_dir,sub_id):
 
             #save confounds
             regressors.to_csv(confreg,sep='\t',index=False)
+
     dcanjosn = {
          "Name": "ABCDDCAN",
          "BIDSVersion": "1.4.0",
