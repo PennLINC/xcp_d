09195f33af960105f0a3d12ee501299ee5ce2c2d
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2021-11-16 22:23:22 -0500

    Update report_core.py

--- xcp_abcd/interfaces/report_core.py
+++ xcp_abcd/interfaces/report_core.py
@@ -54,7 +54,7 @@ def run_reports(
 
 
 def generate_reports(
-    subject_list, fmriprep_dir, work_dir,output_dir, run_uuid, config=None, packagename=None
+    subject_list, fmri_dir, work_dir,output_dir, run_uuid, config=None, packagename=None
 ):
     """Execute run_reports on a list of subjects."""
     #reportlets_dir = None
@@ -72,7 +72,7 @@ def generate_reports(
         for subject_label in subject_list
     ]
 
-    fmriprep_dir = fmriprep_dir
+    fmri_dir = fmri_dir
     errno = sum(report_errors)
     if errno == 0:
         print('xcp_abcd finished without errors')
