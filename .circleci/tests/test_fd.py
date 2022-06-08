430ca8bab15cbea07f3d186beb961e7ee59feef5
kahinimehta <68132139+kahinimehta@users.noreply.github.com>
2022-06-08 13:15:12 -0400

    fixed df slice copy warnings using .iloc

--- .circleci/tests/test_fd.py
+++ .circleci/tests/test_fd.py
@@ -27,25 +27,20 @@ def test_fd():
 
 test_fd()  # Call the function
 
-
+#%%
 def test_fd_interface():  # Checking results
     input_file = '/Users/kahinim/Desktop/FD_test/fmriprep/sub-99964/ses-10105/func/sub-99964_ses-10105_task-rest_acq-singleband_space-MNI152NLin6Asym_res-2_desc-preproc_bold.nii.gz'
     mask = '/Users/kahinim/Desktop/FD_test/fmriprep/sub-99964/ses-10105/func/sub-99964_ses-10105_task-rest_acq-singleband_space-MNI152NLin6Asym_res-2_desc-brain_mask.nii.gz'
     confounds_tsv = '/Users/kahinim/Desktop/FD_test/fmriprep/sub-99964/ses-10105/func/sub-99964_ses-10105_task-rest_acq-singleband_desc-confounds_timeseries.tsv'
     df = pd.read_table(confounds_tsv)
     # Replace confounds tsv values with values that should be omitted
-    df["trans_x"][1:4] = [6, 8, 9]
-    df["trans_y"][5:8] = [7, 8, 9]
-    df["trans_z"][9:12] = [12, 8, 9]
+    df.loc[1:3, "trans_x"] = [6, 8, 9]
+    df.loc[4:6, "trans_y"] = [7, 8, 9]
+    df.loc[7:9, "trans_z"] = [12, 8, 9]
     tmpdir = '/Users/kahinim/Desktop/FD_test'  # So we can see results
     os.chdir(tmpdir)
     confounds_tsv = "edited_" + confounds_tsv.split('/func/')[1]  # Rename with same convention as initial confounds tsv
     df.to_csv(confounds_tsv, sep='\t', index=False)
-    # Confirming that the df values are changed as expected
-    confounds_df = pd.read_table(confounds_tsv)
-    assert confounds_df["trans_x"][1:4].tolist() == [6, 8, 9]
-    assert confounds_df["trans_y"][5:8].tolist() == [7, 8, 9]
-    assert confounds_df["trans_z"][9:12].tolist() == [12, 8, 9]
 
     # Run workflow
     cscrub = censorscrub()
@@ -57,16 +52,17 @@ def test_fd_interface():  # Checking results
     cscrub.inputs.time_todrop = 0
     cscrub.inputs.head_radius = 50
     cscrub.run()
-    # Confirming that the df values were changed as expected
-    confounds_df = pd.read_table(cscrub.inputs.fmriprep_confounds)
-    assert confounds_df["trans_x"][1:4].tolist() == [6, 8, 9]
-    assert confounds_df["trans_y"][5:8].tolist() == [7, 8, 9]
-    assert confounds_df["trans_z"][9:12].tolist() == [12, 8, 9]
+
+    # Confirming that the df values are changed as expected
+    confounds_df = pd.read_table(confounds_tsv)
+    assert confounds_df.loc[1:3, "trans_x"].tolist() == [6, 8, 9]
+    assert confounds_df.loc[4:6, "trans_y"].tolist() == [7, 8, 9]
+    assert confounds_df.loc[7:9, "trans_z"].tolist() == [12, 8, 9]
 
 
 test_fd_interface()  # Call the function
 
-
+#%%
 def test_fd_interface_dummy_signal():  # Checking results with dummy signal
     input_file = '/Users/kahinim/Desktop/FD_test/fmriprep/sub-99964/ses-10105/func/sub-99964_ses-10105_task-rest_acq-singleband_space-MNI152NLin6Asym_res-2_desc-preproc_bold.nii.gz'
     mask = '/Users/kahinim/Desktop/FD_test/fmriprep/sub-99964/ses-10105/func/sub-99964_ses-10105_task-rest_acq-singleband_space-MNI152NLin6Asym_res-2_desc-brain_mask.nii.gz'
@@ -86,3 +82,6 @@ def test_fd_interface_dummy_signal():  # Checking results with dummy signal
 
 
 test_fd_interface_dummy_signal()  # Call the function
+
+
+
