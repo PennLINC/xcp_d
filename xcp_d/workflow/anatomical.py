bd74299eecfd238f03f6e74c6ee7f7c7e8a0075e
kahinimehta <68132139+kahinimehta@users.noreply.github.com>
2022-06-15 11:18:38 -0400

    testing this passes circleci

--- xcp_d/workflow/anatomical.py
+++ xcp_d/workflow/anatomical.py
@@ -357,7 +357,7 @@ def init_anatomical_wf(
             left_wm_surf_wf = pe.Node(CiftiSurfaceResample(new_sphere=left_sphere_fsLR,
                     metric = ' BARYCENTRIC ',in_file=L_wm_surf), name="left_wm_surf",mem_gb=mem_gb,n_procs=omp_nthreads)
             left_pial_surf_wf = pe.Node(CiftiSurfaceResample(new_sphere=left_sphere_fsLR,
-                    metric = ' BARYCENTRIC ',in_file=L_pial_surf), name="left_pial_surf",mem_gb=mem_gb,n_procs=omp_nthreads)
+                    metric = ' BARYCENTRIC ',in_file=L_pial_surf), name="left_pial_surf", mem_gb=mem_gb,n_procs=omp_nthreads)
             left_midthick_surf_wf = pe.Node(CiftiSurfaceResample(new_sphere=left_sphere_fsLR,
                     metric = ' BARYCENTRIC ',in_file=L_midthick_surf), name="left_midthick_surf",mem_gb=mem_gb,n_procs=omp_nthreads)
             left_inf_surf_wf = pe.Node(CiftiSurfaceResample(new_sphere=left_sphere_fsLR,
