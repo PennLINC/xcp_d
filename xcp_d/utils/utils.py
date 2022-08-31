90f95f86e95f8e7595fb74f129ffd55787cdf93b
kahinimehta <kahini_mehta@brown.edu>
2022-08-31 10:27:24 -0400

    fix apply transforms

--- xcp_d/utils/utils.py
+++ xcp_d/utils/utils.py
@@ -39,8 +39,8 @@ def get_transformfilex(bold_file, mni_to_t1w, t1w_to_native):
 
     # in case fMRIPrep outputs are generated in MNI6, as
     # done in case of AROMA outputs
-    elif 'MNI152NLin6Sym' in os.path.basename(mni_to_t1w):
-        template = 'MNI152NLin6Sym'
+    elif 'MNI152NLin6ASym' in os.path.basename(mni_to_t1w):
+        template = 'MNI152NLin6ASym'
 
     # Pull out the correct transforms based on bold_file name
     # and string them together.
