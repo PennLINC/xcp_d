c5554b7925f54b2da176a861bb87e1bc7e86fa4c
Taylor Salo <tsalo006@fiu.edu>
2022-09-02 12:50:14 -0400

    Sort imports.

--- xcp_d/utils/write_save.py
+++ xcp_d/utils/write_save.py
@@ -1,10 +1,12 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """Utilities to read and write nifiti and cifti data."""
-import nibabel as nb
-import numpy as np
 import os
 import subprocess
+
+import nibabel as nb
+import numpy as np
+from nilearn import masking
 from templateflow.api import get as get_template
 
 
