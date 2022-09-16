2588b407d54562db67dfdbe55d5dd4371dee2644
kahinimehta <kahini_mehta@brown.edu>
2022-09-16 13:52:38 -0400

    fix potential circular import issue

--- xcp_d/interfaces/__init__.py
+++ xcp_d/interfaces/__init__.py
@@ -1,29 +1,19 @@
-from xcp_d.interfaces.connectivity import (
-    ApplyTransformsx,
-    NiftiConnect,
-    connectplot,
-    get_atlas_cifti,
-    get_atlas_nifti,
-)
 from xcp_d.interfaces.filtering import FilteringData
-from xcp_d.interfaces.layout_builder import LayoutBuilder
-from xcp_d.interfaces.prepostcleaning import CensorScrub, RemoveTR, interpolate
+from xcp_d.interfaces.regression import regress, ciftidespike
+from xcp_d.interfaces.connectivity import (NiftiConnect, ApplyTransformsx, get_atlas_cifti,
+                                           get_atlas_nifti, connectplot)
+from xcp_d.interfaces.resting_state import computealff, surfaceReho, brainplot
+
+from xcp_d.interfaces.prepostcleaning import interpolate, CensorScrub, RemoveTR
 from xcp_d.interfaces.qc_plot import computeqcplot
-from xcp_d.interfaces.regression import ciftidespike, regress
-from xcp_d.interfaces.report import AboutSummary, FunctionalSummary, SubjectSummary
+from xcp_d.interfaces.report import SubjectSummary, AboutSummary, FunctionalSummary
 from xcp_d.interfaces.report_core import generate_reports
-from xcp_d.interfaces.resting_state import brainplot, computealff, surfaceReho
-from xcp_d.interfaces.surfplotting import (
-    BrainPlotx,
-    PlotImage,
-    PlotSVGData,
-    RegPlot,
-    RibbontoStatmap,
-    SurftoVolume,
-)
+from xcp_d.interfaces.surfplotting import (SurftoVolume, BrainPlotx, PlotSVGData, RegPlot,
+                                           PlotImage, RibbontoStatmap)
+from xcp_d.interfaces.layout_builder import LayoutBuilder
 
 __all__ = [
-    'regress', 'FilteringData', 'NiftiConnect',
+    'regress','FilteringData', 'NiftiConnect',
     'computealff', 'surfaceReho', 'get_atlas_cifti', 'get_atlas_nifti',
     'ApplyTransformsx', 'interpolate', 'CensorScrub', 'RemoveTR',
     'computeqcplot', 'SubjectSummary', 'AboutSummary', 'FunctionalSummary',
