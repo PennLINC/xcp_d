f6b571ba0fd4dbbec00a6daed26c36616a42d2a8
Taylor Salo <tsalo006@fiu.edu>
2022-09-17 12:55:32 -0400

    isort init file.

--- xcp_d/utils/__init__.py
+++ xcp_d/utils/__init__.py
@@ -1,36 +1,62 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """A range of utility functions for xcp_d interfaces and workflows."""
-from xcp_d.utils.write_save import (read_ndata, write_ndata, read_gii, write_gii,
-                                    despikedatacifti)
-from xcp_d.utils.plot import (plot_svg, compute_dvars, plotimage)
-from xcp_d.utils.confounds import load_confound_matrix
-from xcp_d.utils.fcon import (extract_timeseries_funct, compute_2d_reho, compute_alff,
-                              mesh_adjacency)
+from xcp_d.utils.bids import DerivativesDataSink as bid_derivative
+from xcp_d.utils.bids import (
+    collect_data,
+    collect_participants,
+    extract_t1w_seg,
+    select_cifti_bold,
+    select_registrationfile,
+)
 from xcp_d.utils.cifticonnectivity import CiftiCorrelation
 from xcp_d.utils.ciftiparcellation import CiftiParcellate
-from xcp_d.utils.ciftiseparatemetric import CiftiSeparateMetric
 from xcp_d.utils.ciftiresample import CiftiSurfaceResample
-from xcp_d.utils.bids import (collect_participants, collect_data, select_registrationfile,
-                              select_cifti_bold, extract_t1w_seg)
-from xcp_d.utils.bids import DerivativesDataSink as bid_derivative
-from xcp_d.utils.modified_data import (interpolate_masked_data,
-                                       generate_mask, compute_FD)
-from xcp_d.utils.sentry import sentry_setup
-
-from xcp_d.utils.qcmetrics import regisQ
-
-from xcp_d.utils.utils import (get_maskfiles, get_transformfile, get_transformfilex,
-                               stringforparams, fwhm2sigma, get_customfile)
-
-from xcp_d.utils.plot import (plot_svgx, plot_carpet, confoundplot)
-
-from xcp_d.utils.execsummary import (surf2vol, get_regplot, plot_registrationx,
-                                     generate_brain_sprite, ribbon_to_statmap)
-from xcp_d.utils.dcan2fmriprep import dcan2fmriprep
-from xcp_d.utils.hcp2fmriprep import hcp2fmriprep
-from xcp_d.utils.restingstate import ReHoNamePatch, DespikePatch, ContrastEnhancement
+from xcp_d.utils.ciftiseparatemetric import CiftiSeparateMetric
 from xcp_d.utils.concantenation import concatenatebold
+from xcp_d.utils.confounds import load_confound_matrix
+from xcp_d.utils.dcan2fmriprep import dcan2fmriprep
+from xcp_d.utils.execsummary import (
+    generate_brain_sprite,
+    get_regplot,
+    plot_registrationx,
+    ribbon_to_statmap,
+    surf2vol,
+)
+from xcp_d.utils.fcon import (
+    compute_2d_reho,
+    compute_alff,
+    extract_timeseries_funct,
+    mesh_adjacency,
+)
+from xcp_d.utils.hcp2fmriprep import hcp2fmriprep
+from xcp_d.utils.modified_data import compute_FD, generate_mask, interpolate_masked_data
+from xcp_d.utils.plot import (
+    compute_dvars,
+    confoundplot,
+    plot_carpet,
+    plot_svg,
+    plot_svgx,
+    plotimage,
+)
+from xcp_d.utils.qcmetrics import regisQ
+from xcp_d.utils.restingstate import ContrastEnhancement, DespikePatch, ReHoNamePatch
+from xcp_d.utils.sentry import sentry_setup
+from xcp_d.utils.utils import (
+    fwhm2sigma,
+    get_customfile,
+    get_maskfiles,
+    get_transformfile,
+    get_transformfilex,
+    stringforparams,
+)
+from xcp_d.utils.write_save import (
+    despikedatacifti,
+    read_gii,
+    read_ndata,
+    write_gii,
+    write_ndata,
+)
 
 __all__ = [
     'read_ndata',
