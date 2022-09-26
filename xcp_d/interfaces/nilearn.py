49624daecc97c37508aa8daa0c90f75f035df736
Taylor Salo <tsalo006@fiu.edu>
2022-09-26 14:54:17 -0400

    Interfaces for smoothing and math.

--- xcp_d/interfaces/nilearn.py
+++ xcp_d/interfaces/nilearn.py
@@ -7,6 +7,7 @@ from nipype.interfaces.base import (
     InputMultiPath,
     SimpleInterface,
     TraitedSpec,
+    traits,
 )
 from nipype.interfaces.nilearn import NilearnBaseInterface
 
@@ -21,7 +22,7 @@ class _MergeInputSpec(BaseInterfaceInputSpec):
         "concat_4d.nii.gz",
         usedefault=True,
         exists=False,
-        desc="The name of the concatenated file to write out. concate_4d.nii.gz by default.",
+        desc="The name of the concatenated file to write out. concat_4d.nii.gz by default.",
     )
 
 
@@ -44,5 +45,92 @@ class Merge(NilearnBaseInterface, SimpleInterface):
         img_4d = concat_imgs(self.inputs.in_files)
         self._results["out_file"] = os.path.join(runtime.cwd, self.inputs.out_file)
         img_4d.to_filename(self._results["out_file"])
+
+        return runtime
+
+
+class _SmoothInputSpec(BaseInterfaceInputSpec):
+    in_file = File(
+        exists=True,
+        mandatory=True,
+        desc="An image to smooth.",
+    )
+    fwhm = traits.Either(
+        traits.Float(),
+        traits.List(
+            traits.Float(),
+            minlen=3,
+            maxlen=3,
+        ),
+        desc="Smoothing strength, as a full-width at half maximum, in millimeters.",
+    )
+    out_file = File(
+        "smooth_img.nii.gz",
+        usedefault=True,
+        exists=False,
+        desc="The name of the smoothed file to write out. smooth_img.nii.gz by default.",
+    )
+
+
+class _SmoothOutputSpec(TraitedSpec):
+    out_file = File(
+        exists=True,
+        desc="Smoothed output file.",
+    )
+
+
+class Smooth(NilearnBaseInterface, SimpleInterface):
+    """Smooth image."""
+
+    input_spec = _SmoothInputSpec
+    output_spec = _SmoothOutputSpec
+
+    def _run_interface(self, runtime):
+        from nilearn.image import smooth_img
+
+        img_smoothed = smooth_img(self.inputs.in_file, fwhm=self.inputs.fwhm)
+        self._results["out_file"] = os.path.join(runtime.cwd, self.inputs.out_file)
+        img_smoothed.to_filename(self._results["out_file"])
+
+        return runtime
+
+
+class _SmoothInputSpec(BaseInterfaceInputSpec):
+    in_file = File(
+        exists=True,
+        mandatory=True,
+        desc="An image to do math on.",
+    )
+    expression = traits.String(
+        mandatory=True,
+        desc="A mathematical expression to apply to the image. Must have 'img' in it."
+    )
+    out_file = File(
+        "out_img.nii.gz",
+        usedefault=True,
+        exists=False,
+        desc="The name of the mathified file to write out. out_img.nii.gz by default.",
+    )
+
+
+class _SmoothOutputSpec(TraitedSpec):
+    out_file = File(
+        exists=True,
+        desc="Mathified output file.",
+    )
+
+
+class BinaryMath(NilearnBaseInterface, SimpleInterface):
+    """Do math on an image."""
+
+    input_spec = _SmoothInputSpec
+    output_spec = _SmoothOutputSpec
+
+    def _run_interface(self, runtime):
+        from nilearn.image import math_img
+
+        img_mathed = math_img(self.inputs.expression, img=self.inputs.in_file)
+        self._results["out_file"] = os.path.join(runtime.cwd, self.inputs.out_file)
+        img_mathed.to_filename(self._results["out_file"])
 
         return runtime
