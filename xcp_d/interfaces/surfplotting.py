801ffd05f52329af1c091c010c65059a35b510d5
Kahini Mehta <68132139+kahinimehta@users.noreply.github.com>
2022-10-10 15:33:46 -0400

    Update surfplotting.py

--- xcp_d/interfaces/surfplotting.py
+++ xcp_d/interfaces/surfplotting.py
@@ -1,9 +1,5 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-
-
-
-
 Surface plotting interfaces."""
 
 from nipype import logging
