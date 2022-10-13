6ea42e4bd25546954ac870a47b3f07cb6adfaed3
kahinimehta <kahini_mehta@brown.edu>
2022-10-13 10:58:48 -0400

    detailed ALFF comments

--- .circleci/tests/test_ALFF.py
+++ .circleci/tests/test_ALFF.py
@@ -17,7 +17,7 @@ def test_nifti_alff(data_dir, tmp_path_factory):
     """
     Test ALFF computations as done for Niftis.
 
-    Get the FFT of a Nifti, add to its lower frequencies
+    Get the FFT of a Nifti, add to the amplitude of its lower frequencies
     and confirm the mean ALFF after addition to lower frequencies
     has increased.
     """
@@ -49,20 +49,23 @@ def test_nifti_alff(data_dir, tmp_path_factory):
     alff_compute_wf.inputs.inputnode.bold_mask = bold_mask
     alff_compute_wf.inputs.inputnode.clean_bold = bold_file
     alff_compute_wf.run()
-    # Let's get the mean of the data for later testing
+    # Let's get the mean of the ALFF for later comparison
     original_alff = os.path.join(
         tempdir, "compute_alff_wf/alff_compt/sub-color",
         "nest001_ses-1_task-rest_run-1_space-MNI152NLin2009cAsym_desc-",
         "preproc_bold_alff.nii.gz"
     )
     original_alff_data_mean = nb.load(original_alff).get_fdata().mean()
-    # Now let's do an FFT to get some fake data in the low
-    # frequency regions of a certain voxel
+    # Now let's do an FFT 
     original_bold_data = read_ndata(bold_file, bold_mask)
+    # Let's work with a single voxel
     voxel_data = original_bold_data[2, :]
     fft_data = fft(voxel_data)
     mean = fft_data.mean()
+    # Let's increase the values of the first few frequency's amplitudes
+    # to create fake data
     fft_data[:, :11] += 300 * mean
+    # Let's convert this back into time domain
     changed_voxel_data = ifft(fft_data)
     # Let's replace the original value with the fake data
     original_bold_data[2, :] = changed_voxel_data
@@ -71,7 +74,9 @@ def test_nifti_alff(data_dir, tmp_path_factory):
     write_ndata(
         original_bold_data, template=bold_file, mask=bold_mask, filename=filename
     )
-    # Now let's compute ALFF for the new file and see how it compares
+    # Now let's compute ALFF for the new file and see how it compares 
+    # to the original ALFF - it should increase since we increased
+    # the amplitude in low frequencies for a voxel
     tempdir = tmp_path_factory.mktemp("test_ALFF_nifti_dir2")
     alff_compute_wf.base_dir = tempdir
     alff_compute_wf.inputs.inputnode.bold_mask = bold_mask
@@ -89,7 +94,7 @@ def test_cifti_alff(data_dir, tmp_path_factory):
     """
     Test ALFF computations as done for Ciftis.
 
-    Get the FFT of a Cifti, add to its lower frequencies
+    Get the FFT of a Cifti, add to the amplitude of its lower frequencies
     and confirm the ALFF after addition to lower frequencies
     has changed in the expected direction.
     """
@@ -120,20 +125,22 @@ def test_cifti_alff(data_dir, tmp_path_factory):
     alff_compute_wf.inputs.inputnode.bold_mask = bold_mask
     alff_compute_wf.inputs.inputnode.clean_bold = bold_file
     alff_compute_wf.run()
-    # Let's get the mean of the data for later testing
+    # Let's get the mean of the data for later comparison
     original_alff = os.path.join(
         tempdir, "compute_alff_wf/alff_compt/sub-color",
         "nest001_ses-1_task-rest_run-2_space-fsLR_den-91k_",
         "bold_alff.dtseries.nii"
     )
     original_alff_data_mean = nb.load(original_alff).get_fdata().mean()
-    # Now let's do an FFT to get some fake data in the low
-    # frequency regions of a certain voxel
+    # Now let's do an FFT 
     original_bold_data = read_ndata(bold_file, bold_mask)
+    # Let's work with a single voxel
     voxel_data = original_bold_data[2, :]
     fft_data = fft(voxel_data)
     mean = fft_data.mean()
+    # Let's increase the amplitudes for the lower frequencies
     fft_data[:, :11] += 300 * mean
+    # Let's get this back into the time domain
     changed_voxel_data = ifft(fft_data)
     # Let's replace the original value with the fake data
     original_bold_data[2, :] = changed_voxel_data
@@ -154,6 +161,7 @@ def test_cifti_alff(data_dir, tmp_path_factory):
         tempdir + "compute_alff_wf/alff_compt/editedfile_alff.dtseries.nii"
     )
     new_alff_data_mean = nb.load(new_alff).get_fdata().mean()
-    # Now let's make sure ALFF has increased ...
+    # Now let's make sure ALFF has increased, as we added
+    # to the amplitude of the lower frequencies in a voxel
     assert new_alff_data_mean > original_alff_data_mean
     return
