6a8fdafdd05f9c9b6e62d39b7750cf533fdab7b6
kahinimehta <kahini_mehta@brown.edu>
2022-10-24 14:37:56 -0400

    update derivatives path

--- .circleci/Nibabies.sh
+++ .circleci/Nibabies.sh
@@ -28,7 +28,7 @@ TESTNAME=nibabies
 setup_dir ${TESTDIR}/${TESTNAME}
 TEMPDIR=${TESTDIR}/${TESTNAME}/work
 OUTPUT_DIR=${TESTDIR}/${TESTNAME}/derivatives
-BIDS_INPUT_DIR=${TESTDIR}/data/nibabies_test_data
+BIDS_INPUT_DIR=${TESTDIR}/data/nibabies_test_data/derivatives/nibabies
 XCPD_CMD=$(run_xcpd_cmd ${BIDS_INPUT_DIR} ${OUTPUT_DIR} ${TEMPDIR})
 
 $XCPD_CMD \
