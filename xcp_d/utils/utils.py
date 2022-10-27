cf7256c4934093f6af02cd389e1d8066416a3786
Taylor Salo <tsalo006@fiu.edu>
2022-10-27 10:45:46 -0400

    Invert sample_mask.

--- xcp_d/utils/utils.py
+++ xcp_d/utils/utils.py
@@ -669,8 +669,8 @@ def _denoise_with_nilearn(
 
     confounds_df = pd.read_table(confounds_file)
 
-    sample_mask_bool = pd.read_table(censoring_file)["framewise_displacement"].values
-    sample_mask = np.where(sample_mask_bool)[0]
+    sample_mask_bool = pd.read_table(censoring_file)["framewise_displacement"].values.astype(bool)
+    sample_mask = np.where(~sample_mask_bool)[0]
 
     print(f"raw_data: {raw_data.shape}")
     print(f"sample_mask: {sample_mask}")
