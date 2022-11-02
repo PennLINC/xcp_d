e5a74827e94ab22cc4e0321609e64ba1bdd94a51
Taylor Salo <salot@pennmedicine.upenn.edu>
2022-11-02 08:58:31 -0400

    Only generate ALFF derivatives if bandpass filtering is enabled (#628)

--- .circleci/NiftiWithoutFreeSurferTest.sh
+++ .circleci/NiftiWithoutFreeSurferTest.sh
@@ -27,8 +27,9 @@ BIDS_INPUT_DIR=${TESTDIR}/data/fmriprepwithoutfreesurfer/fmriprep
 XCPD_CMD=$(run_xcpd_cmd ${BIDS_INPUT_DIR} ${OUTPUT_DIR} ${TEMPDIR})
 
 $XCPD_CMD \
-    --despike  --head_radius 40 \
-    --smoothing 6  -f 100 -v -v \
-    --nuissance-regressors 27P
+    --despike --head_radius 40 \
+    --smoothing 6 -f 100 -vv \
+    --nuissance-regressors 27P \
+    --disable-bandpass-filter
 
 echo $XCPD_CMD
