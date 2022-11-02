22d6066f949e95297c016fcb696770ccb4c0a2df
Steven Meisler <smeisler@g.harvard.edu>
2022-11-01 20:46:16 -0400

    Fix MNI152Lin6Sym import in utils.py
    I presumed the original duplicate MNI152NLin6Asym template finding was a mistake.

--- xcp_d/utils/utils.py
+++ xcp_d/utils/utils.py
@@ -148,8 +148,8 @@ def get_transformfilex(bold_file, mni_to_t1w, t1w_to_native):
     elif 'MNI152NLin6Asym' in os.path.basename(mni_to_t1w):
         template = 'MNI152NLin6Asym'
 
-    elif 'MNI152NLin6Asym' in os.path.basename(mni_to_t1w):
-        template = 'MNI152NLin6Asym'
+    elif 'MNI152NLin6Sym' in os.path.basename(mni_to_t1w):
+        template = 'MNI152NLin6Sym'
 
     # Pull out the correct transforms based on bold_file name
     # and string them together.
