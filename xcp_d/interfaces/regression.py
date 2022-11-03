daf1513664e01c1bcf22fda56fcaecf95c37f210
Taylor Salo <tsalo006@fiu.edu>
2022-11-03 10:33:02 -0400

    Fix linter issue.

--- xcp_d/interfaces/regression.py
+++ xcp_d/interfaces/regression.py
@@ -1,8 +1,6 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """Regression interfaces."""
-from os.path import exists
-
 import pandas as pd
 from nipype import logging
 from nipype.interfaces.base import (
