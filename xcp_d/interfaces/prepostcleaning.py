956f69e0a13df3db6d57352fc86759d519724196
Taylor Salo <tsalo006@fiu.edu>
2022-11-10 12:38:20 -0500

    Fix custom confounds dummytime drop.

--- xcp_d/interfaces/prepostcleaning.py
+++ xcp_d/interfaces/prepostcleaning.py
@@ -134,10 +134,8 @@ class RemoveTR(SimpleInterface):
 
         # Drop the first N rows from the custom confounds file, if provided:
         if self.inputs.custom_confounds:
-            custom_confounds_tsv_undropped = pd.read_table(self.inputs.custom_confounds)
-            custom_confounds_tsv_dropped = custom_confounds_tsv_undropped.loc[
-                np.arange(volumes_to_drop)
-            ]
+            custom_confounds_df = pd.read_table(self.inputs.custom_confounds)
+            custom_confounds_tsv_dropped = custom_confounds_df.drop[np.arange(volumes_to_drop)]
         else:
             LOGGER.warning("No custom confounds were found or had their volumes dropped.")
 
