a7c0e51a0da1ea33a1f6bc198adea3d02a9e7712
Taylor Salo <tsalo006@fiu.edu>
2022-11-14 12:18:35 -0500

    Hopefully fix tests.

--- .circleci/tests/test_Reg.py
+++ .circleci/tests/test_Reg.py
@@ -9,11 +9,13 @@ from xcp_d.utils.write_save import read_ndata
 from xcp_d.utils.confounds import load_confound_matrix
 
 
-def test_Reg_Nifti(data_dir):
+def test_regression_nifti(data_dir, tmp_path_factory):
     """Test NIFTI regression."""
-    data_dir = os.path.join(data_dir,
-                            "fmriprepwithfreesurfer")
-    #  Specify inputs
+    data_dir = os.path.join(data_dir, "fmriprepwithfreesurfer")
+    temp_dir = tmp_path_factory.mktemp("test_regression_nifti")
+
+    # Specify inputs
+    TR = 0.5
     in_file = (
         data_dir + "/fmriprep/sub-colornest001/ses-1/func/"
         "sub-colornest001_ses-1_task-rest_run-1_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz"
@@ -22,24 +24,27 @@ def test_Reg_Nifti(data_dir):
         data_dir + "/fmriprep/sub-colornest001/ses-1/func/"
         "sub-colornest001_ses-1_task-rest_run-2_desc-confounds_timeseries.tsv"
     )
-    TR = 0.5
     mask = (
         data_dir + "/fmriprep/sub-colornest001/ses-1/func/"
         "sub-colornest001_ses-1_task-rest_run-1_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz"
     )
+
+    # Read in confounds. Confounds must be selected before running Regress.
+    df = load_confound_matrix(confound_tsv=confounds, params="36P")
+    assert df.shape[1] == 36
+    selected_confounds_file = os.path.join(temp_dir, "temp.tsv")
+    df.to_csv(selected_confounds_file, sep="\t", index=False)
+
     # Run regression
-    test_nifti = Regress(
+    regression = Regress(
         mask=mask,
         in_file=in_file,
         original_file=in_file,
-        confounds=confounds,
+        confounds=selected_confounds_file,
         TR=TR,
         params="36P",
     )
-    results = test_nifti.run()
-    # Read in confounds
-    df = load_confound_matrix(confound_tsv=confounds, params="36P")
-    assert df.shape[1] == 36
+    results = regression.run(cwd=temp_dir)
 
     # Loop through each column in the confounds matrix, creating a list of
     # regressors for correlation
@@ -63,39 +68,47 @@ def test_Reg_Nifti(data_dir):
     assert (max(regressed_correlations)) < 0.01
 
 
-def test_Reg_Cifti(data_dir):
+def test_regression_cifti(data_dir, tmp_path_factory):
     """Test CIFTI regression."""
     # Specify inputs
-    data_dir = os.path.join(data_dir,
-                            "fmriprepwithfreesurfer")
-    in_file = (
-        data_dir + "/fmriprep/sub-colornest001/ses-1/func/"
-        "sub-colornest001_ses-1_task-rest_run-1_space-fsLR_den-91k_bold.dtseries.nii"
-    )
-    confounds = (
-        data_dir + "/fmriprep/sub-colornest001/ses-1/func/"
-        "sub-colornest001_ses-1_task-rest_run-1_desc-confounds_timeseries.tsv"
-    )
+    data_dir = os.path.join(data_dir, "fmriprepwithfreesurfer")
+    temp_dir = tmp_path_factory.mktemp("test_regression_cifti")
+
     TR = 0.5
-    mask = (
-        data_dir + "/fmriprep/sub-colornest001/ses-1/func/"
-        "sub-colornest001_ses-1_task-rest_run-1_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz"
+    in_file = os.path.join(
+        data_dir,
+        "fmriprep/sub-colornest001/ses-1/func",
+        "sub-colornest001_ses-1_task-rest_run-1_space-fsLR_den-91k_bold.dtseries.nii",
     )
+    confounds = os.path.join(
+        data_dir,
+        "fmriprep/sub-colornest001/ses-1/func",
+        "sub-colornest001_ses-1_task-rest_run-1_desc-confounds_timeseries.tsv",
+    )
+    mask = os.path.join(
+        data_dir,
+        "fmriprep/sub-colornest001/ses-1/func",
+        "sub-colornest001_ses-1_task-rest_run-1_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz",
+    )
+
+    # Read in confounds. Confounds must be selected before running Regress.
+    df = load_confound_matrix(confound_tsv=confounds, params="36P")
+    assert df.shape[1] == 36
+    selected_confounds_file = os.path.join(temp_dir, "temp.tsv")
+    df.to_csv(selected_confounds_file, sep="\t", index=False)
+
     # Run regression
-    test_cifti = Regress(
+    regression = Regress(
         mask=mask,
         in_file=in_file,
         original_file=in_file,
-        confounds=confounds,
+        confounds=selected_confounds_file,
         TR=TR,
         params="36P",
     )
+    regression.base_dir = temp_dir
+    results = regression.run(cwd=temp_dir)
 
-    results = test_cifti.run()
-
-    # Read in confounds
-    df = load_confound_matrix(confound_tsv=confounds, params="36P")
-    assert df.shape[1] == 36
     # Loop through each column in the confounds matrix, creating a list of
     # regressors for correlation
     list_of_regressors = []
--- .circleci/tests/test_TR.py
+++ .circleci/tests/test_TR.py
@@ -17,22 +17,27 @@ from xcp_d.interfaces.prepostcleaning import RemoveTR
 def test_RemoveTR_nifti(data_dir):
     """Test RemoveTR() for NIFTI input data."""
     # Define inputs
-    data_dir = os.path.join(data_dir,
-                            "fmriprepwithoutfreesurfer/fmriprep/")
-    boldfile = data_dir + "sub-01/func/" \
+    data_dir = os.path.join(data_dir, "fmriprepwithoutfreesurfer/fmriprep/")
+    boldfile = (
+        data_dir + "sub-01/func/"
         "sub-01_task-mixedgamblestask_run-1_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz"
-    confounds_file = data_dir + "sub-01/func/" \
+    )
+    confounds_file = (
+        data_dir + "sub-01/func/"
         "sub-01_task-mixedgamblestask_run-1_desc-confounds_timeseries.tsv"
+    )
 
     # Find the original number of volumes acc. to nifti & confounds timeseries
-    original_confounds = pd.read_csv(confounds_file, sep="\t")
+    original_confounds = pd.read_table(confounds_file)
     original_nvols_nifti = nb.load(boldfile).get_fdata().shape[3]
 
     # Test a nifti file with 0 volumes to remove
     remove_nothing = RemoveTR(
         bold_file=boldfile,
         fmriprep_confounds_file=confounds_file,
-        dummy_scans=0)
+        confounds_file=confounds_file,
+        dummy_scans=0,
+    )
     results = remove_nothing.run()
     undropped_confounds = pd.read_table(results.outputs.fmriprep_confounds_file_dropped_TR)
     # Were the files created?
@@ -41,15 +46,15 @@ def test_RemoveTR_nifti(data_dir):
     # Have the confounds stayed the same shape?
     assert undropped_confounds.shape == original_confounds.shape
     # Has the nifti stayed the same shape?
-    assert nb.load(results.
-                   outputs.bold_file_dropped_TR).get_fdata().shape[3] == original_nvols_nifti
+    assert (
+        nb.load(results.outputs.bold_file_dropped_TR).get_fdata().shape[3] == original_nvols_nifti
+    )
 
     # Test a nifti file with 1-10 volumes to remove
     for n in range(0, 10):
         remove_n_vols = RemoveTR(
-            bold_file=boldfile,
-            fmriprep_confounds_file=confounds_file,
-            dummy_scans=n)
+            bold_file=boldfile, fmriprep_confounds_file=confounds_file, dummy_scans=n
+        )
         results = remove_n_vols.run()
         dropped_confounds = pd.read_table(results.outputs.fmriprep_confounds_file_dropped_TR)
         # Were the files created?
@@ -59,8 +64,10 @@ def test_RemoveTR_nifti(data_dir):
         assert dropped_confounds.shape[0] == original_confounds.shape[0] - n
         # Has the nifti changed correctly?
         try:
-            assert nb.load(results.outputs.bold_file_dropped_TR).get_fdata().shape[3]\
+            assert (
+                nb.load(results.outputs.bold_file_dropped_TR).get_fdata().shape[3]
                 == original_nvols_nifti - n
+            )
         except Exception as exc:
             exc = nb.load(results.outputs.bold_file_dropped_TR).get_fdata().shape[3]
             print(f"Tests failing at N = {n}.")
@@ -70,22 +77,27 @@ def test_RemoveTR_nifti(data_dir):
 def test_RemoveTR_cifti(data_dir):
     """Test RemoveTR() for CIFTI input data."""
     # Define inputs
-    data_dir = os.path.join(data_dir,
-                            "fmriprepwithfreesurfer")
-    boldfile = data_dir + "/fmriprep/sub-colornest001/ses-1/func/" \
+    data_dir = os.path.join(data_dir, "fmriprepwithfreesurfer")
+    boldfile = (
+        data_dir + "/fmriprep/sub-colornest001/ses-1/func/"
         "sub-colornest001_ses-1_task-rest_run-1_space-fsLR_den-91k_bold.dtseries.nii"
-    confounds_file = data_dir + "/fmriprep/sub-colornest001/ses-1/func/" \
+    )
+    confounds_file = (
+        data_dir + "/fmriprep/sub-colornest001/ses-1/func/"
         "sub-colornest001_ses-1_task-rest_run-1_desc-confounds_timeseries.tsv"
+    )
 
     # Find the original number of volumes acc. to cifti & confounds timeseries
-    original_confounds = pd.read_csv(confounds_file, sep="\t")
+    original_confounds = pd.read_table(confounds_file)
     original_nvols_cifti = nb.load(boldfile).get_fdata().shape[0]
 
     # Test a cifti file with 0 volumes to remove
     remove_nothing = RemoveTR(
         bold_file=boldfile,
         fmriprep_confounds_file=confounds_file,
-        dummy_scans=0)
+        confounds_file=confounds_file,
+        dummy_scans=0,
+    )
     results = remove_nothing.run()
     undropped_confounds = pd.read_table(results.outputs.fmriprep_confounds_file_dropped_TR)
     # Were the files created?
@@ -94,16 +106,16 @@ def test_RemoveTR_cifti(data_dir):
     # Have the confounds stayed the same shape?
     assert undropped_confounds.shape == original_confounds.shape
     # Has the cifti stayed the same shape?
-    assert nb.load(results.outputs.bold_file_dropped_TR).get_fdata(
-    ).shape[0] == original_nvols_cifti
+    assert (
+        nb.load(results.outputs.bold_file_dropped_TR).get_fdata().shape[0] == original_nvols_cifti
+    )
 
     # Test a cifti file with 1-10 volumes to remove
     for n in range(0, 10):
         remove_n_vols = RemoveTR(
-            bold_file=boldfile,
-            fmriprep_confounds_file=confounds_file,
-            dummy_scans=n)
-#         print(n)
+            bold_file=boldfile, fmriprep_confounds_file=confounds_file, dummy_scans=n
+        )
+        #         print(n)
         results = remove_n_vols.run()
         dropped_confounds = pd.read_table(results.outputs.fmriprep_confounds_file_dropped_TR)
         # Were the files created?
@@ -113,12 +125,15 @@ def test_RemoveTR_cifti(data_dir):
         assert dropped_confounds.shape[0] == original_confounds.shape[0] - n
         # Has the cifti changed correctly?
         try:
-            assert nb.load(results.outputs.bold_file_dropped_TR).get_fdata().shape[0]\
+            assert (
+                nb.load(results.outputs.bold_file_dropped_TR).get_fdata().shape[0]
                 == original_nvols_cifti - n
+            )
         except Exception as exc:
             exc = nb.load(results.outputs.bold_file_dropped_TR).get_fdata().shape[0]
             print(f"Tests failing at N = {n}.")
             raise Exception(f"Number of volumes in dropped cifti is {exc}.")
+
 
 # Testing with CUSTOM CONFOUNDS
 # Note: I had to test this locally as I don't have the permissions to share the
