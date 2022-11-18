7443f254daa67149ca1dde70d26ff67b810731c9
kahinimehta <kahini_mehta@brown.edu>
2022-11-18 16:34:36 -0500

    fix attempt

--- .circleci/tests/test_confounds.py
+++ .circleci/tests/test_confounds.py
@@ -10,18 +10,21 @@ from xcp_d.utils.confounds import load_confound_matrix
 
 def test_custom_confounds(data_dir, tmp_path_factory):
     """Ensure that custom confounds can be loaded without issue."""
-    bold_file = os.path.join(
-        data_dir,
-        (
-            "fmriprep/sub-colornest001/ses-1/func/"
-            "sub-colornest001_ses-1_task-rest_run-1"
-            "_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz"
-        )
-    )
+
 
     tempdir = tmp_path_factory.mktemp("test_custom_confounds")
 
     data_dir = os.path.join(data_dir, "fmriprepwithfreesurfer")
+
+    bold_file = os.path.join(
+    data_dir,
+    (
+        "fmriprep/sub-colornest001/ses-1/func/"
+        "sub-colornest001_ses-1_task-rest_run-1"
+        "_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz"
+    )
+)
+
     N_VOLUMES = 184
     TR = 2.5
 
