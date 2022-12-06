2554d8426229be6441f2b1286e5f9d026be87181
kahinimehta <kahini_mehta@brown.edu>
2022-12-06 14:23:54 -0500

    format

--- xcp_d/utils/hcp2fmriprep.py
+++ xcp_d/utils/hcp2fmriprep.py
@@ -227,7 +227,7 @@ def hcpfmriprepx(hcp_dir, out_dir, sub_id):
         regressors["white_matter_derivative1_power2"] = regressors["white_matter_derivative1"] ** 2
     
         regressors["white_matter_power2"] = regressors["white_matter"]**2
-        regressors["csf_power2"] =  regressors["csf"]**2
+        regressors["csf_power2"] = regressors["csf"]**2
         regressors["csf_derivative1"] = pd.DataFrame(
             np.diff(regressors["csf"].to_numpy(), prepend=0)
         )
