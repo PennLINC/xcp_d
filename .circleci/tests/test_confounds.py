94319b7698ecb7a03712fd5077659b8c5977e3aa
Taylor Salo <tsalo006@fiu.edu>
2022-12-18 10:06:46 -0500

    Test confound shapes.

--- .circleci/tests/test_confounds.py
+++ .circleci/tests/test_confounds.py
@@ -13,15 +13,15 @@ def test_custom_confounds(fmriprep_with_freesurfer_data, tmp_path_factory):
     tempdir = tmp_path_factory.mktemp("test_custom_confounds")
     bold_file = fmriprep_with_freesurfer_data["nifti_file"]
 
-    N_VOLUMES = 184
+    N_VOLUMES = 60
     TR = 2.5
 
     frame_times = np.arange(N_VOLUMES) * TR
     events_df = pd.DataFrame(
         {
-            "onset": [10, 30, 50, 70, 90, 110, 130, 150],
-            "duration": [5, 10, 5, 10, 5, 10, 5, 10],
-            "trial_type": (["condition01"] * 4) + (["condition02"] * 4)
+            "onset": [10, 30, 50],
+            "duration": [5, 10, 5],
+            "trial_type": (["condition01"] * 2) + (["condition02"] * 1)
         },
     )
     custom_confounds = make_first_level_design_matrix(
@@ -47,6 +47,34 @@ def test_custom_confounds(fmriprep_with_freesurfer_data, tmp_path_factory):
         custom_confounds=custom_confounds_file,
     )
     # We expect n params + 2 (one for each condition in custom confounds)
-    assert combined_confounds.shape == (184, 26)
+    assert combined_confounds.shape == (N_VOLUMES, 26)
     assert "condition01" in combined_confounds.columns
     assert "condition02" in combined_confounds.columns
+
+
+def test_confounds_strategies(fmriprep_with_freesurfer_data):
+    """Ensure that xcp_d loads the right confounds."""
+    bold_file = fmriprep_with_freesurfer_data["nifti_file"]
+
+    N_VOLUMES = 60
+
+    confounds_df = load_confound_matrix(params="24P", img_file=bold_file)
+    assert confounds_df.shape == (N_VOLUMES, 24)
+
+    confounds_df = load_confound_matrix(params="27P", img_file=bold_file)
+    assert confounds_df.shape == (N_VOLUMES, 27)
+
+    confounds_df = load_confound_matrix(params="36P", img_file=bold_file)
+    assert confounds_df.shape == (N_VOLUMES, 36)
+
+    confounds_df = load_confound_matrix(params="acompcor", img_file=bold_file)
+    assert confounds_df.shape == (N_VOLUMES, 18)
+
+    confounds_df = load_confound_matrix(params="acompcor_gsr", img_file=bold_file)
+    assert confounds_df.shape == (N_VOLUMES, 19)
+
+    confounds_df = load_confound_matrix(params="aroma", img_file=bold_file)
+    assert confounds_df.shape == (N_VOLUMES, 48)
+
+    confounds_df = load_confound_matrix(params="aroma_gsr", img_file=bold_file)
+    assert confounds_df.shape == (N_VOLUMES, 49)
