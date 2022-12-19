94f036ea2cbf85c6d5bf82be54e1db6d87147466
Taylor Salo <tsalo006@fiu.edu>
2022-12-19 09:30:27 -0500

    Copy surface collection from #706.
    Update base.py
    
    Update base.py
    
    Update base.py
    
    Update bids.py

--- xcp_d/workflow/base.py
+++ xcp_d/workflow/base.py
@@ -21,6 +21,7 @@ from xcp_d.interfaces.bids import DerivativesDataSink
 from xcp_d.interfaces.report import AboutSummary, SubjectSummary
 from xcp_d.utils.bids import (
     collect_data,
+    collect_surface_data,
     get_preproc_pipeline_info,
     write_dataset_description,
 )
@@ -333,6 +334,11 @@ def init_subject_wf(
     )
     LOGGER.debug(f"Collected data:\n{yaml.dump(subj_data, default_flow_style=False, indent=4)}")
 
+    surface_data, standard_space_surfaces, surfaces_found = collect_surface_data(
+        layout=layout,
+        participant_label=subject_id,
+    )
+
     # determine the appropriate post-processing workflow
     postproc_wf_function = init_ciftipostprocess_wf if cifti else init_boldpostprocess_wf
     preproc_files = subj_data["bold"]
@@ -346,6 +352,18 @@ def init_subject_wf(
                 "t1w_seg",
                 "template_to_t1w_xform",
                 "t1w_to_template_xform",
+                # surface files
+                "lh_pial_surf",
+                "rh_pial_surf",
+                "lh_smoothwm_surf",
+                "rh_smoothwm_surf",
+                # hcp-style surface files
+                "lh_midthickness_surf",
+                "rh_midthickness_surf",
+                "lh_inflated_surf",
+                "rh_inflated_surf",
+                "lh_vinflated_surf",
+                "rh_vinflated_surf",
             ],
         ),
         name="inputnode",
@@ -356,6 +374,20 @@ def init_subject_wf(
     inputnode.inputs.t1w_seg = subj_data["t1w_seg"]
     inputnode.inputs.template_to_t1w_xform = subj_data["template_to_t1w_xform"]
     inputnode.inputs.t1w_to_template_xform = subj_data["t1w_to_template_xform"]
+
+    # surface files (required for brainsprite/warp workflows)
+    inputnode.inputs.lh_pial_surf = surface_data["lh_pial_surf"]
+    inputnode.inputs.rh_pial_surf = surface_data["rh_pial_surf"]
+    inputnode.inputs.lh_smoothwm_surf = surface_data["lh_smoothwm_surf"]
+    inputnode.inputs.rh_smoothwm_surf = surface_data["rh_smoothwm_surf"]
+
+    # optional surface files
+    inputnode.inputs.lh_midthickness_surf = surface_data["lh_midthickness_surf"]
+    inputnode.inputs.rh_midthickness_surf = surface_data["rh_midthickness_surf"]
+    inputnode.inputs.lh_inflated_surf = surface_data["lh_inflated_surf"]
+    inputnode.inputs.rh_inflated_surf = surface_data["rh_inflated_surf"]
+    inputnode.inputs.lh_inflated_surf = surface_data["lh_vinflated_surf"]
+    inputnode.inputs.rh_inflated_surf = surface_data["rh_vinflated_surf"]
 
     workflow = Workflow(name=name)
 
