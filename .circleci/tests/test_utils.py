7ae5364815cd362a76c1ba043c09cdc68588cf73
Taylor Salo <tsalo006@fiu.edu>
2023-01-17 08:59:09 -0500

    Write test.

--- .circleci/tests/test_utils.py
+++ .circleci/tests/test_utils.py
@@ -1,1 +1,10 @@
 """Init tests for utils."""
+from xcp_d.utils.utils import estimate_brain_radius
+
+
+def test_estimate_brain_radius(fmriprep_with_freesurfer_data):
+    """Ensure that the brain radius estimation function returns the right value."""
+    bold_mask = fmriprep_with_freesurfer_data["brain_mask_file"]
+
+    radius = estimate_brain_radius(bold_mask)
+    assert radius == 78.12350298308195
