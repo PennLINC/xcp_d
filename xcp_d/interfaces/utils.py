583ebf3b34b8334a3707a575a793faa8edbf3e74
Taylor Salo <salot@pennmedicine.upenn.edu>
2023-01-20 08:34:25 -0500

    Refactor anatomical workflow (#706)
    Co-authored-by: Kahini Mehta <68132139+kahinimehta@users.noreply.github.com>

---
+++ xcp_d/interfaces/utils.py
@@ -0,0 +1,41 @@
+"""Miscellaneous utility interfaces."""
+from nipype.interfaces.base import (
+    BaseInterfaceInputSpec,
+    InputMultiObject,
+    OutputMultiObject,
+    SimpleInterface,
+    TraitedSpec,
+    traits,
+    traits_extension,
+)
+
+
+class _FilterUndefinedInputSpec(BaseInterfaceInputSpec):
+    inlist = InputMultiObject(
+        traits.Str,
+        mandatory=True,
+        desc="List of objects to filter.",
+    )
+
+
+class _FilterUndefinedOutputSpec(TraitedSpec):
+    outlist = OutputMultiObject(
+        traits.Str,
+        desc="Filtered list of objects.",
+    )
+
+
+class FilterUndefined(SimpleInterface):
+    """Extract timeseries and compute connectivity matrices."""
+
+    input_spec = _FilterUndefinedInputSpec
+    output_spec = _FilterUndefinedOutputSpec
+
+    def _run_interface(self, runtime):
+        inlist = self.inputs.inlist
+        outlist = []
+        for item in inlist:
+            if item is not None and traits_extension.isdefined(item):
+                outlist.append(item)
+        self._results["outlist"] = outlist
+        return runtime
