6c58702fb1eaa751f915a323b8c886f04a078363
Taylor Salo <tsalo006@fiu.edu>
2023-02-06 17:25:11 -0500

    Use ApplyTransforms, not FixHeaderApplyTransforms.

--- xcp_d/workflows/plotting.py
+++ xcp_d/workflows/plotting.py
@@ -1,9 +1,9 @@
 """Plotting workflows."""
 from nipype import Function
 from nipype.interfaces import utility as niu
+from nipype.interfaces.ants.resampling import ApplyTransforms
 from nipype.pipeline import engine as pe
 from niworkflows.engine.workflows import LiterateWorkflow as Workflow
-from niworkflows.interfaces.fixes import FixHeaderApplyTransforms as ApplyTransforms
 from templateflow.api import get as get_template
 
 from xcp_d.interfaces.bids import DerivativesDataSink
