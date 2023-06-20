6ed960e0a571e6674465c31c644a86b811a8b55d
Taylor Salo <tsalo006@fiu.edu>
2023-06-20 16:41:23 -0400

    Add tests for new interface and function.

--- xcp_d/tests/test_interfaces_censoring.py
+++ xcp_d/tests/test_interfaces_censoring.py
@@ -1,9 +1,10 @@
 """Tests for framewise displacement calculation."""
 import os
 
+import numpy as np
 import pandas as pd
 
-from xcp_d.interfaces.censoring import GenerateConfounds
+from xcp_d.interfaces import censoring
 
 
 def test_generate_confounds(fmriprep_with_freesurfer_data, tmp_path_factory):
@@ -25,7 +26,7 @@ def test_generate_confounds(fmriprep_with_freesurfer_data, tmp_path_factory):
     df.to_csv(confounds_tsv, sep="\t", index=False, header=True)
 
     # Run workflow
-    interface = GenerateConfounds(
+    interface = censoring.GenerateConfounds(
         in_file=in_file,
         params="24P",
         TR=0.8,
@@ -45,3 +46,45 @@ def test_generate_confounds(fmriprep_with_freesurfer_data, tmp_path_factory):
     assert os.path.isfile(results.outputs.confounds_file)
     assert os.path.isfile(results.outputs.motion_file)
     assert os.path.isfile(results.outputs.temporal_mask)
+
+
+def test_random_censor(tmp_path_factory):
+    """Test RandomCensor."""
+    tmpdir = tmp_path_factory.mktemp("test_random_censor")
+    n_volumes, n_outliers = 500, 100
+    exact_scans = [100, 200, 300, 400]
+
+    outliers_arr = np.zeros(n_volumes, dtype=int)
+    rng = np.random.default_rng(0)
+    outlier_idx = rng.choice(np.arange(n_volumes, dtype=int), size=n_outliers, replace=False)
+    outliers_arr[outlier_idx] = 1
+    temporal_mask_df = pd.DataFrame(data=outliers_arr, columns=["framewise_displacement"])
+    original_temporal_mask = os.path.join(tmpdir, "orig_tmask.tsv")
+    temporal_mask_df.to_csv(original_temporal_mask, index=False, sep="\t")
+
+    # Run the RandomCensor interface without any exact_scans.
+    interface = censoring.RandomCensor(
+        temporal_mask_metadata={},
+        temporal_mask=original_temporal_mask,
+        exact_scans=exact_scans,
+        random_seed=0,
+    )
+    results = interface.run(cwd=tmpdir)
+    assert results.outputs.temporal_mask == original_temporal_mask  # same file as input
+    assert isinstance(results.outputs.temporal_mask_metadata, dict)
+
+    # Run the interface with exact_scans
+    interface = censoring.RandomCensor(
+        temporal_mask_metadata={},
+        temporal_mask=original_temporal_mask,
+        exact_scans=exact_scans,
+        random_seed=0,
+    )
+    results = interface.run(cwd=tmpdir)
+    assert os.path.isfile(results.outputs.temporal_mask)
+    assert isinstance(results.outputs.temporal_mask_metadata, dict)
+    new_temporal_mask_df = pd.read_table(results.outputs.temporal_mask)
+    for exact_scan in exact_scans:
+        exact_scan_col = f"exact_{exact_scan}"
+        assert exact_scan_col in new_temporal_mask_df.columns
+        assert new_temporal_mask_df[exact_scan_col].sum() == n_volumes - (n_outliers + exact_scan)
