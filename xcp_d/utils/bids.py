f5b5eb8649c7973d652098e4ff82792ed3b63867
Taylor Salo <salot@pennmedicine.upenn.edu>
2023-06-26 11:11:01 -0400

    Allow white matter surface suffix to be either "_smoothwm" or "_white" (#899)

--- xcp_d/utils/bids.py
+++ xcp_d/utils/bids.py
@@ -478,13 +478,13 @@ def collect_surface_data(layout, participant_label):
         "lh_wm_surf": {
             "hemi": "L",
             "desc": None,
-            "suffix": "smoothwm",
+            "suffix": ["smoothwm", "white"],
             "extension": ".surf.gii",
         },
         "rh_wm_surf": {
             "hemi": "R",
             "desc": None,
-            "suffix": "smoothwm",
+            "suffix": ["smoothwm", "white"],
             "extension": ".surf.gii",
         },
     }
