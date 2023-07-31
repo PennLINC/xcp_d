491a8d32b38a8dca4ea3cf11f4b7d725731875af
Taylor Salo <tsalo006@fiu.edu>
2023-07-31 12:08:43 -0400

    Use ITK version of transform.

--- xcp_d/utils/utils.py
+++ xcp_d/utils/utils.py
@@ -222,7 +222,7 @@ def get_std2bold_xfms(bold_file, inverted=False):
             # MNIInfant --> NLin6
             MNI152Infant_to_MNI152NLin6Asym = pkgrf(
                 "xcp_d",
-                "data/transform/tpl-MNI152NLin6Asym_from-MNIInfant_xfm.mat",
+                "data/transform/tpl-MNI152NLin6Asym_from-MNIInfant_mode-image_xfm.mat",
             )
             transform_list = [MNI152Infant_to_MNI152NLin6Asym]
 
