7339313b632784b122fb6e3dedada77f199145d4
Taylor Salo <tsalo006@fiu.edu>
2023-09-19 15:23:06 -0400

    Update execsummary.py

--- xcp_d/interfaces/execsummary.py
+++ xcp_d/interfaces/execsummary.py
@@ -215,10 +215,9 @@ class ExecutiveSummary(object):
             # Convert any floats in the name to ints
             temp_dict = task_entity_namer[i_set]
             temp_dict = {
-                key: int(value) if isinstance(value, float) and value.is_integer() else value
+                key: int(value) if np.isscalar(value) and value.is_integer() else value
                 for key, value in temp_dict.items()
             }
-            raise Exception(temp_dict)
             task_file_figures["key"] = "_".join([f"{k}-{v}" for k, v in temp_dict.items()])
 
             query = {
