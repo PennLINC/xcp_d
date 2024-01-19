0c858e002f7e24e56a6242496d0f0557038d1f78
Taylor Salo <tsalo006@fiu.edu>
2024-01-19 14:02:52 -0500

    Update test_utils_write_save.py

--- xcp_d/tests/test_utils_write_save.py
+++ xcp_d/tests/test_utils_write_save.py
@@ -91,5 +91,8 @@ def test_write_ndata(ds001419_data, tmp_path_factory):
 
     # Try using a txt file as a template
     fake_template = os.path.join(tmpdir, "file.txt")
+    with open(fake_template, "w") as fo:
+        fo.write("TEST")
+
     with pytest.raises(ValueError, match="Unknown extension"):
         write_save.write_ndata(cifti_data, template=fake_template, filename=temp_cifti)
