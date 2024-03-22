1742de22a901717fcad198551279015cbc0e01da
Taylor Salo <tsalo006@fiu.edu>
2024-03-22 15:48:24 -0400

    Copy over BIDSURI code I wrote for fMRIPrep.

--- xcp_d/interfaces/bids.py
+++ xcp_d/interfaces/bids.py
@@ -8,19 +8,25 @@ from pathlib import Path
 import nibabel as nb
 import numpy as np
 from bids.layout import Config
+from bids.utils import listify
 from nipype import logging
 from nipype.interfaces.base import (
     BaseInterfaceInputSpec,
     Directory,
+    DynamicTraitedSpec,
     File,
     SimpleInterface,
     TraitedSpec,
+    isdefined,
     traits,
 )
+from nipype.interfaces.io import add_traits
+from nipype.interfaces.utility.base import _ravel
 from niworkflows.interfaces.bids import DerivativesDataSink as BaseDerivativesDataSink
 from pkg_resources import resource_filename as pkgrf
 
-from xcp_d.utils.bids import get_entity
+from xcp_d.utils.bids import _find_nearest_path, get_entity
+
 
 # NOTE: Modified for xcpd's purposes
 xcp_d_spec = loads(Path(pkgrf("xcp_d", "data/xcp_d_bids_config.json")).read_text())
@@ -308,5 +314,51 @@ class CopyAtlas(SimpleInterface):
             shutil.copyfile(in_file, out_file)
 
         self._results["out_file"] = out_file
+
+        return runtime
+
+
+class _BIDSURIInputSpec(DynamicTraitedSpec):
+    dataset_links = traits.Dict(mandatory=True, desc="Dataset links")
+    out_dir = traits.Str(mandatory=True, desc="Output directory")
+
+
+class _BIDSURIOutputSpec(TraitedSpec):
+    out = traits.List(
+        traits.Str,
+        desc="BIDS URI(s) for file",
+    )
+
+
+class BIDSURI(SimpleInterface):
+    """Convert input filenames to BIDS URIs, based on links in the dataset.
+
+    This interface can combine multiple lists of inputs.
+    """
+
+    input_spec = _BIDSURIInputSpec
+    output_spec = _BIDSURIOutputSpec
+
+    def __init__(self, numinputs=0, **inputs):
+        super().__init__(**inputs)
+        self._numinputs = numinputs
+        if numinputs >= 1:
+            input_names = [f"in{i + 1}" for i in range(numinputs)]
+        else:
+            input_names = []
+        add_traits(self.inputs, input_names)
+
+    def _run_interface(self, runtime):
+        inputs = [getattr(self.inputs, f"in{i + 1}") for i in range(self._numinputs)]
+        in_files = listify(inputs)
+        in_files = _ravel(in_files)
+        # Remove undefined inputs
+        in_files = [f for f in in_files if isdefined(f)]
+        # Convert the dataset links to BIDS URI prefixes
+        updated_keys = {f"bids:{k}:": Path(v) for k, v in self.inputs.dataset_links.items()}
+        updated_keys["bids::"] = Path(self.inputs.out_dir)
+        # Convert the paths to BIDS URIs
+        out = [_find_nearest_path(updated_keys, f) for f in in_files]
+        self._results["out"] = out
 
         return runtime
