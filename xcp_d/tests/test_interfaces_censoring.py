df787ca84947cc942ca6937ee4da4ef0d41a7868
Taylor Salo <tsalo006@fiu.edu>
2024-05-02 10:46:34 -0400

    Update test_interfaces_censoring.py

--- xcp_d/tests/test_interfaces_censoring.py
+++ xcp_d/tests/test_interfaces_censoring.py
@@ -52,7 +52,7 @@ def test_generate_confounds(ds001419_data, tmp_path_factory):
         censor_between=[1, 1],
         head_radius=50,
         full_confounds=confounds_tsv,
-        fmriprep_confounds_json=confounds_json,
+        full_confounds_json=confounds_json,
         custom_confounds_file=custom_confounds_file,
         motion_filter_type=None,
         motion_filter_order=4,
