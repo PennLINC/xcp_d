2f3a3b466751e10d18add5dc24c14e5a6927c335
Taylor Salo <tsalo006@fiu.edu>
2024-06-27 11:31:18 -0400

    Whoops

--- xcp_d/tests/test_cli.py
+++ xcp_d/tests/test_cli.py
@@ -52,7 +52,6 @@ def test_ds001419_nifti(data_dir, output_dir, working_dir):
         "--smoothing=6",
         "--motion-filter-type=lp",
         "--band-stop-min=6",
-        "--skip-parcellation",
         "--skip-dcan-qc",
         "--random-seed=8675309",
         "--min-time=100",
