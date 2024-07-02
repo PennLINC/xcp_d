c4143074b5614d65e6cf8b33edfd623e5623d2ef
Taylor Salo <tsalo006@fiu.edu>
2024-07-02 14:15:37 -0400

    Update atlas.py

--- xcp_d/utils/atlas.py
+++ xcp_d/utils/atlas.py
@@ -102,7 +102,7 @@ def get_atlas_nifti(atlas):
             load_data(f"atlases/tpl-MNI152NLin6Asym_atlas-{atlas}_dseg.json")
         )
 
-    if not isfile(atlas_file) and isfile(atlas_labels_file) and isfile(atlas_metadata_file):
+    if not (isfile(atlas_file) and isfile(atlas_labels_file) and isfile(atlas_metadata_file)):
         raise FileNotFoundError(
             f"File(s) DNE:\n\t{atlas_file}\n\t{atlas_labels_file}\n\t{atlas_metadata_file}"
         )
@@ -168,7 +168,7 @@ def get_atlas_cifti(atlas):
         atlas_labels_file = str(load_data(f"atlases/atlas-{atlas}_dseg.tsv"))
         atlas_metadata_file = str(load_data(f"atlases/tpl-fsLR_atlas-{atlas}_dseg.json"))
 
-    if not isfile(atlas_file) and isfile(atlas_labels_file) and isfile(atlas_metadata_file):
+    if not (isfile(atlas_file) and isfile(atlas_labels_file) and isfile(atlas_metadata_file)):
         raise FileNotFoundError(
             f"File(s) DNE:\n\t{atlas_file}\n\t{atlas_labels_file}\n\t{atlas_metadata_file}"
         )
