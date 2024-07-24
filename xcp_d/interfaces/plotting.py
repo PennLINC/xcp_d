14e39d066d1850d5b2a08398f423d4da1b80b3aa
Taylor Salo <tsalo006@fiu.edu>
2024-07-24 15:58:14 -0400

    Update plotting.py

--- xcp_d/interfaces/plotting.py
+++ xcp_d/interfaces/plotting.py
@@ -11,10 +11,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import svgutils.transform as sg
-from matplotlib.cm import ScalarMappable
-from matplotlib.colors import Normalize
-from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
-from nilearn.plotting import plot_anat, plot_stat_map, plot_surf_stat_map
+from nilearn.plotting import plot_anat, plot_stat_map
 from nipype import logging
 from nipype.interfaces.base import (
     BaseInterfaceInputSpec,
@@ -1183,106 +1180,31 @@ class PlotDenseCifti(SimpleInterface):
             rh = self.inputs.rh_underlay
             lh = self.inputs.lh_underlay
 
-        cifti = nb.load(self.inputs.in_file)
-        cifti_data = cifti.get_fdata()
-        cifti_axes = [cifti.header.get_axis(i) for i in range(cifti.ndim)]
+        data_img = nb.load(self.inputs.in_file)
+        img_data = data_img.get_fdata()
+        data_axes = [data_img.header.get_axis(i) for i in range(data_img.ndim)]
 
-        # Create Figure and GridSpec.
-        fig = plt.figure(constrained_layout=False)
-        fig.set_size_inches(6.5, 6)
-        # Add an additional column for the colorbar
-        gs = GridSpec(1, 2, figure=fig, width_ratios=[1, 0.05])
-        subplot_gridspec = gs[0, 0]
-        subplot = fig.add_subplot(subplot_gridspec)
-        colorbar_gridspec = gs[0, 1]
-
-        subplot.set_axis_off()
-
-        # Create 4 Axes (2 rows, 2 columns) from the subplot
-        gs_inner = GridSpecFromSubplotSpec(2, 2, subplot_spec=subplot_gridspec)
-        inner_subplots = [
-            fig.add_subplot(gs_inner[i, j], projection="3d") for i in range(2) for j in range(2)
-        ]
-
-        lh_surf_data = surf_data_from_cifti(
-            cifti_data,
-            cifti_axes[1],
+        plot_obj = Plot(lh, rh)
+        lh_data = surf_data_from_cifti(
+            img_data,
+            data_axes[1],
             "CIFTI_STRUCTURE_CORTEX_LEFT",
         )
-        rh_surf_data = surf_data_from_cifti(
-            cifti_data,
-            cifti_axes[1],
+        rh_data = surf_data_from_cifti(
+            img_data,
+            data_axes[1],
             "CIFTI_STRUCTURE_CORTEX_RIGHT",
         )
+        vmax = np.nanmax([np.nanmax(lh_data), np.nanmax(rh_data)])
+        vmin = np.nanmin([np.nanmin(lh_data), np.nanmin(rh_data)])
 
-        vmax = np.nanmax([np.nanmax(lh_surf_data), np.nanmax(rh_surf_data)])
-        vmin = np.nanmin([np.nanmin(lh_surf_data), np.nanmin(rh_surf_data)])
-
-        plot_surf_stat_map(
-            lh,
-            lh_surf_data,
-            vmin=vmin,
-            vmax=vmax,
-            hemi="left",
-            view="lateral",
-            engine="matplotlib",
+        plot_obj.add_layer(
+            {"left": np.squeeze(lh_data), "right": np.squeeze(rh_data)},
             cmap="cool",
-            colorbar=False,
-            axes=inner_subplots[0],
-            figure=fig,
+            color_range=(vmin, vmax),
+            cbar=True,
         )
-        plot_surf_stat_map(
-            rh,
-            rh_surf_data,
-            vmin=vmin,
-            vmax=vmax,
-            hemi="right",
-            view="lateral",
-            engine="matplotlib",
-            cmap="cool",
-            colorbar=False,
-            axes=inner_subplots[1],
-            figure=fig,
-        )
-        plot_surf_stat_map(
-            lh,
-            lh_surf_data,
-            vmin=vmin,
-            vmax=vmax,
-            hemi="left",
-            view="medial",
-            engine="matplotlib",
-            cmap="cool",
-            colorbar=False,
-            axes=inner_subplots[2],
-            figure=fig,
-        )
-        plot_surf_stat_map(
-            rh,
-            rh_surf_data,
-            vmin=vmin,
-            vmax=vmax,
-            hemi="right",
-            view="medial",
-            engine="matplotlib",
-            cmap="cool",
-            colorbar=False,
-            axes=inner_subplots[3],
-            figure=fig,
-        )
-
-        inner_subplots[0].set_title("Left Hemisphere", fontsize=10)
-        inner_subplots[1].set_title("Right Hemisphere", fontsize=10)
-
-        for ax in inner_subplots:
-            ax.set_rasterized(True)
-
-        # Create a ScalarMappable with the "cool" colormap and the specified vmin and vmax
-        sm = ScalarMappable(cmap="cool", norm=Normalize(vmin=vmin, vmax=vmax))
-
-        colorbar_ax = fig.add_subplot(colorbar_gridspec)
-        # Add a colorbar to colorbar_ax using the ScalarMappable
-        fig.colorbar(sm, cax=colorbar_ax)
+        fig = plot_obj.build()
 
         self._results["out_file"] = fname_presuffix(
             self.inputs.in_file,
