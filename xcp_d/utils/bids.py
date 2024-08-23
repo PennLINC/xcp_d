b440a0ddcb7d908b9272b4f4b57fde2868ad5ebf
Taylor Salo <tsalo006@fiu.edu>
2024-08-23 15:36:26 -0400

    Update bids.py

--- xcp_d/utils/bids.py
+++ xcp_d/utils/bids.py
@@ -284,11 +284,29 @@ def collect_data(
             )
             queries["t1w"]["space"] = "T2w"
             if not temp_t1w_files:
-                LOGGER.warning("No T2w-space T1w found. Enabling T2w-only processing.")
-                queries["template_to_anat_xfm"]["to"] = "T2w"
-                queries["anat_to_template_xfm"]["from"] = "T2w"
-                # Nibabies may include space-T2w for some derivatives
-                queries["anat_dseg"]["space"] = ["T2w", None]
+                LOGGER.warning("No T2w-space T1w found. Attempting T2w-only processing.")
+                temp_query = queries["anat_to_template_xfm"].copy()
+                temp_query["from"] = "T2w"
+                temp_xfm_files = layout.get(
+                    return_type="file",
+                    subject=participant_label,
+                    **temp_query,
+                )
+                if not temp_xfm_files:
+                    LOGGER.warning(
+                        "T2w-to-template transform not found. Attempting T1w-only processing."
+                    )
+                    queries["t1w"]["space"] = ["T1w", None]
+                    queries["template_to_anat_xfm"]["to"] = "T1w"
+                    queries["anat_to_template_xfm"]["from"] = "T1w"
+                    # Nibabies may include space-T1w for some derivatives
+                    queries["anat_dseg"]["space"] = ["T1w", None]
+                else:
+                    LOGGER.info("Performing T2w-only processing.")
+                    queries["template_to_anat_xfm"]["to"] = "T2w"
+                    queries["anat_to_template_xfm"]["from"] = "T2w"
+                    # Nibabies may include space-T2w for some derivatives
+                    queries["anat_dseg"]["space"] = ["T2w", None]
             else:
                 LOGGER.warning("T2w-space T1w found. Processing anatomical images in T2w space.")
         else:
