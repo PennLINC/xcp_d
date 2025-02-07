c89cbfec85e7a9e5a280e5ac9cad9419b72ac170
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-02-07 11:14:01 -0500

    The combo works, but no need to include it.

--- xcp_d/tests/test_cli.py
+++ xcp_d/tests/test_cli.py
@@ -68,7 +68,7 @@ def test_ds001419_nifti(data_dir, output_dir, working_dir):
         '--output-type=censored',
         '--combine-runs=y',
         '--linc-qc=y',
-        '--abcc-qc=y',
+        '--abcc-qc=n',
         '--despike=n',
         '--file-format=nifti',
         '--input-type=fmriprep',
