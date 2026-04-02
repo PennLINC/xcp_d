256200cf01cae9b3c51742c6c09b70b630408c8a
Taylor Salo <salot@pennmedicine.upenn.edu>
2026-04-02 11:43:45 -0400

    Fix atlas BIDSLayout (#1611)

--- xcp_d/utils/atlas.py
+++ xcp_d/utils/atlas.py
@@ -121,7 +121,11 @@ def collect_atlases(datasets, atlases, file_format, bids_filters=None):
     atlas_cache = {}
     for dataset_name, dataset_path in datasets.items():
         if not isinstance(dataset_path, BIDSLayout):
-            layout = BIDSLayout(dataset_path, config=[atlas_cfg], validate=False)
+            layout = BIDSLayout(
+                dataset_path,
+                config=['bids', 'derivatives', atlas_cfg],
+                validate=False,
+            )
         else:
             layout = dataset_path
 
