ef4bc232efe5b8f9a530961af0309e4ea45bddac
Taylor Salo <salot@pennmedicine.upenn.edu>
2026-05-20 09:21:23 -0400

    Add kwargs to _warnings.py.
    Reproduces nipreps/fmriprep#3483.

--- xcp_d/_warnings.py
+++ xcp_d/_warnings.py
@@ -7,17 +7,20 @@ _wlog = logging.getLogger('py.warnings')
 _wlog.addHandler(logging.NullHandler())
 
 
-def _warn(message, category=None, stacklevel=1, source=None):
+def _warn(message, category=None, stacklevel=1, source=None, **kwargs):
     """Redefine the warning function."""
     if category is not None:
         category = type(category).__name__
         category = category.replace('type', 'WARNING')
 
+    if kwargs:
+        logging.getLogger('py.warnings').warning(f'Extra warning kwargs: {kwargs}')
+
     logging.getLogger('py.warnings').warning(f'{category or "WARNING"}: {message}')
 
 
-def _showwarning(message, category, filename, lineno, file=None, line=None):
-    _warn(message, category=category)
+def _showwarning(message, category, filename, lineno, file=None, line=None, **kwargs):
+    _warn(message, category=category, **kwargs)
 
 
 warnings.warn = _warn
